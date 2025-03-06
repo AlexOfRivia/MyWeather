0dbbcaf614927e5f4139d94da7afc61436f4a33b
Alexander Bhandari <31488157+AlexOfRivia@users.noreply.github.com>
2025-03-06 22:37:47 +0100

    initial commit

---
+++ src/MainController.java
@@ -0,0 +1,39 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    @FXML
+    private Label cityLabel; //Label with the searched city name
+
+    @FXML
+    private TextField cityTextField; //Text field for inputing the city
+
+    @FXML
+    private ImageView conditionsImage; //Image with the weather conditions
+
+    @FXML
+    private Button searchButton; //Button to search the city
+
+    @FXML
+    private Label temperatureLabel; //Label with the current temperature
+
+    private Stage mainWindow; //Primary stage
+
+    public void setMainWindow(Stage mainWindow) 
+    {
+        this.mainWindow = mainWindow;
+    }
+
+    @FXML
+    void searchCity(ActionEvent event) 
+    {
+
+    }
+
+}
